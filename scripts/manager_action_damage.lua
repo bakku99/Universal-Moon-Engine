e4428833e09dee4e04a9746e3aa1a00b8ca9c27d
FablesofMalkoros <basecaleb@gmail.com>
2025-11-19 08:44:40 -0600

    CoreRPG /scripts directory

---
+++ scripts/manager_action_damage.lua
@@ -0,0 +1,17 @@
+--
+-- Please see the license.html file included with this distribution for
+-- attribution and copyright information.
+--
+
+function encodeActionText(rAction)
+	return ActionCore.encodeActionText(rAction, "action_damage_tag");
+end
+function decodeRollData(rRoll)
+	ActionCore.decodeRollData(rRoll, "action_damage_tag");
+end
+function decodeRangeText(s)
+	return ActionCore.decodeRangeText(s, "action_damage_tag");
+end
+function decodeLabelText(s)
+	return ActionCore.decodeLabelText(s, "action_damage_tag");
+end
